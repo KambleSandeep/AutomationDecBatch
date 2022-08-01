c6fcbbfe0ec8f3363e7ffec083c855278e511b4b
KambleSandeep <100525445+KambleSandeep@users.noreply.github.com>
2022-08-01 09:37:35 +0530

    PROJECT FILE
    FILE

---
+++ abs/Sample.java
@@ -0,0 +1,19 @@
+package abs;
+
+import Loop.Eveen;
+
+public class Sample extends Test {
+public void m2()
+{
+	
+System.out.println("Test Method-2");
+}
+public static void main(String[] args) {
+
+Sample s=new Sample();
+
+
+s.m1();
+s.m2();
+}
+}
